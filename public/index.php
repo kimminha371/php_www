4725ccde233ec9624e0606158b78205bba4ca3f9
kimminha371 <kkk_92112@naver.com>
2019-10-30 11:16:55 +0900

    DB 접속 및 테이블 표시

--- public/index.php
+++ public/index.php
@@ -1,2 +1,18 @@
 <?php
+$config = include "../dbconf.php";
 echo "대림대학교";
+print_r($config);
+
+require "../Module/Database/Database.php";
+require "../Module/Database/Table.php";
+
+$db = new Database($config);
+echo "<br>";
+$query = "SHOW TABLES";
+$result = $db->queryExecute($query);
+
+$count = mysqli_num_rows($result);
+for($i=0;$i<$count;$i++) {
+    $row = mysqli_fetch_object($result);
+    echo $row->Tables_in_php."<br>";
+}
