9541d306bfad06e4367614b162b66111cc0d2a33
kimminha371 <kkk_92112@naver.com>
2019-11-06 12:49:49 +0900

    insert

--- App/Controller/Select.php
+++ App/Controller/Select.php
@@ -3,25 +3,65 @@ namespace App\Controller;
 class Select
 {
     private $db;
+    private $HttpUri;
+    private $Html;
+
     //생성자
     public function __construct($db)
     {
         //echo __CLASS__; 
         $this->db = $db;
+        $this->HttpUri = new \Module\Http\Uri();
+        $this->Html = new \Module\Html\HtmlTable;
     }
 
+    // 처음 동작 구분 처리
     public function main()
     {
-        $html = new \Module\Html\HtmlTable;
+        $tableName = $this->HttpUri->second();
+        if($this->HttpUri->third() == "new"){
+            echo "새로운 데이터 입력";
+            $this->newInsert($tableName);
+        }else {
+            $this->list($tableName);
+        }
+    }
 
-        $uri = $_SERVER['REQUEST_URI'];
-        $uris = explode("/",$uri);//파란책
-        // []/[select]/[members]
+    // 새로운 데이터를 입력
+    public function newInsert($tableName)
+    {
+        print_r($_POST);
+        if($_POST) {
+            $query = "INSERT INTO ".$tableName." (`FirstName`,`LastName`) 
+                        VALUES ('".$_POST['firstname']."','".$_POST['lastname']."')";
+            $result = $this->db->queryExecute($query);
 
-        if (isset($uris[2])&&$uris[2]) {
+            //페이지 이동
+            header("location:"."/select/".$tableName);
+        }
+        //echo "메인 호출이에요";
+        $query = "DESC ".$tableName;
+        $result =$this->db->queryExecute($query);
 
+        $count = mysqli_num_rows($result);
+        $content = ""; //초기화
+        $rows = []; //배열 초기화
+        for($i=0;$i<$count;$i++) {
+            $row = mysqli_fetch_object($result);
+            $rows []=$row; //배열추가
+        }
+        $content = $this->Html->table($rows);
+
+        $body = file_get_contents("../Resource/insert.html");
+        $body = str_replace("{{content}}",$content,$body); //데이터 치환
+        echo $body;
+    }
         
-        $query = "SELECT * from ".$uris[2]; // SQL 쿼리문
+
+    public function list($tableName) 
+    {
+        if ($tableName) {
+        $query = "SELECT * from ".$tableName; // SQL 쿼리문
         $result =$this->db->queryExecute($query);
 
         $content = ""; //초기화
@@ -35,21 +75,17 @@ class Select
                 //print_r($row);
                 $rows [] = $row;
         }
-            $content = $html->table($rows);
+            $content = $this->Html->table($rows);
         } else {
             // 데이터가 없음
             $content = "데이터 없음";
         }
     } else {
         $content = "선택된 테이블이 없습니다.";
-    }
-
-        
+    }   
         
         $body = file_get_contents("../Resource/select.html");
-        //데이터 치환
-        $body = str_replace("{{content}}",$content,$body); 
+        $body = str_replace("{{content}}",$content,$body); //데이터 치환
         echo $body;
-}
-
+    }
 }
