82d53e75941ee0d48efea69f29d74e06d2ff505d
kimminha371 <kkk_92112@naver.com>
2019-11-06 11:15:24 +0900

    select

--- App/Controller/Databases.php
+++ App/Controller/Databases.php
@@ -1,24 +1,19 @@
 <?php
 namespace App\Controller;
-class TableInfo
+class Databases
 {
     private $db;
     //생성자
     public function __construct($db)
     {
-        //echo __CLASS__; 
+        //echo __CLASS__;
         $this->db = $db;
     }
 
     public function main()
     {
         $html = new \Module\Html\HtmlTable;
-
-        $uri = $_SERVER['REQUEST_URI'];
-        $uris = explode("/",$uri);//파란책
-
-        //echo "메인 호출이에요";
-        $query = "DESC ".$uris[2];
+        $query = "SHOW DATABASES";
         $result =$this->db->queryExecute($query);
 
         $count = mysqli_num_rows($result);
@@ -26,14 +21,20 @@ class TableInfo
         $rows = []; //배열 초기화
         for($i=0;$i<$count;$i++) {
             $row = mysqli_fetch_object($result);
-            $rows []=$row; //배열추가
-    }
+            //$rows []=$row; //배열 추가
+            // 배열 + 배열 = 2차원 배열
+            //키, 값 연상배열
+            $rows []= [
+                'num'=>$i,
+                'name'=>"<a href='/Tables/".$row->Database."'>".$row->Database."</a>"
+            ];
+        }
         $content = $html->table($rows);
-        
-        $body = file_get_contents("../Resource/desc.html");
+
+        $body = file_get_contents("../Resource/database.html");
         //데이터 치환
         $body = str_replace("{{content}}",$content,$body); 
         echo $body;
-}
 
+    }
 }
